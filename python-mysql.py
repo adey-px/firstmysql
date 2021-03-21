6630eccb7385b676b8b498ee287b64018dac00f7
adey-px <aosteph@gmail.com>
2021-03-21 22:04:49 +0000

    added updated files

--- python-mysql.py
+++ python-mysql.py
@@ -1,16 +1,17 @@
 import os
 import pymysql
 
+# Get your databse username
 username = os.getenv('C9_USER')
 
+# connect to the database
 connection = pymysql.connect(host='localhost', user=username, password='', db='Chinook')
 
 try:
+    # Run a query
     with connection.cursor() as cursor:
-        sql = "select * from Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = cursor.execute("delete from Friends where name = %s;", 'Mike')
+        connection.commit()
 finally:
+    # Close the connection, regardless of whether the above was successful
     connection.close()
-
